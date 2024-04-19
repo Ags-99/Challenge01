90a83c04cae3d138848fc0f20bd20e2b5282bea0
Ags-99 <paredesagustin477@gmail.com>
2024-04-19 17:26:20 -0300

    arregalando el css

--- app.js
+++ app.js
@@ -3,45 +3,51 @@ function encriptar() {
     let texto = document.getElementById("texto").value;
     let textoEncriptado = "";
 
-    if(texto === ""){
+    if (texto === "") {
         document.getElementById("position").style.display = "block";
         document.getElementById("btn-copiar").style.display = "none";
-    }else{
-    for (let i = 0; i < texto.length; i++) {
-        if (texto === texto.toUpperCase() || texto[0] === texto[0].toUpperCase()) {
-            if (texto[0] === texto[0].toUpperCase()) {
-                alert("¡Por favor, evita ingresar la palabra completa en mayúsculas!");
-            } else {
-                alert("¡Por favor, evita ingresar letras mayúsculas al inicio de la palabra!");
+        document.getElementById("msj").value = textoEncriptado;
+        return;
+    } else {
+
+        for (let i = 0; i < texto.length; i++) {
+            if (texto[i] === texto[i].toUpperCase()) {
+                alert("¡Por favor, evita ingresar letras mayúsculas!");
+                return; // Detener la ejecución de la función si se encuentra una letra mayúscula
             }
-            return; // Detener la ejecución de la función
-        }  
-        switch(texto[i]) {
-            case "a":
-                textoEncriptado += "ai";
-                break;
-            case "e":
-                textoEncriptado += "enter";
-                break;
-            case "i":
-                textoEncriptado += "imes";
-                break;
-            case "o":
-                textoEncriptado += "ober";
-                break;
-            case "u":
-                textoEncriptado += "ufat";
-                break;
-            default:
-                textoEncriptado += texto[i];
-                break;
+        }
+        for (let i = 0; i < texto.length; i++) {
+            switch (texto[i]) {
+                case "a":
+                    textoEncriptado += "ai";
+                    break;
+                case "e":
+                    textoEncriptado += "enter";
+                    break;
+                case "i":
+                    textoEncriptado += "imes";
+                    break;
+                case "o":
+                    textoEncriptado += "ober";
+                    break;
+                case "u":
+                    textoEncriptado += "ufat";
+                    break;
+                default:
+                    textoEncriptado += texto[i];
+                    break;
+            }
+            document.getElementById("position").style.display = "none";
+            document.getElementById("btn-copiar").style.display = "block";
+            document.getElementById("msj").value = textoEncriptado;
+            document.getElementById("texto").value = "";
+
+            let historialItem = document.createElement("li");
+            historialItem.textContent = "Texto encriptado: " + textoEncriptado;
+            document.querySelector(".historial").appendChild(historialItem);
         }
     }
-    document.getElementById("position").style.display = "none";
-    document.getElementById("btn-copiar").style.display = "block";
-    document.getElementById("msj").value = textoEncriptado;
-    }
-    console.log(textoEncriptado);
+    return;
 }
 
 /*hola - hoberlai*/
@@ -50,42 +56,70 @@ function desencriptar() {
     let textoEncriptado = document.getElementById("texto").value;
     let textoDesencriptado = "";
 
-    if(textoEncriptado === ""){
+    if (textoEncriptado === "") {
         document.getElementById("position").style.display = "block";
         document.getElementById("btn-copiar").style.display = "none";
-    }else{
-    for (let i = 0; i < textoEncriptado.length; i++) {
-        if (textoEncriptado[i] === "a" && textoEncriptado[i + 1] === "i") {
-            textoDesencriptado += "a";
-            i += 1; // Avanza el índice para evitar duplicados
-        } else if (textoEncriptado[i] === "e" && textoEncriptado.substring(i, i + 5) === "enter") {
-            textoDesencriptado += "e";
-            i += 4; 
-        } else if (textoEncriptado[i] === "i" && textoEncriptado.substring(i, i + 4) === "imes") {
-            textoDesencriptado += "i";
-            i += 3; 
-        } else if (textoEncriptado[i] === "o" && textoEncriptado.substring(i, i + 4) === "ober") {
-            textoDesencriptado += "o";
-            i += 3; 
-        } else if (textoEncriptado[i] === "u" && textoEncriptado.substring(i, i + 4) === "ufat") {
-            textoDesencriptado += "u";
-            i += 3; 
-        } else {
-            textoDesencriptado += textoEncriptado[i]; 
+        document.getElementById("msj").value = textoDesencriptado;
+    } else {
+        for (let i = 0; i < textoEncriptado.length; i++) {
+            if (textoEncriptado[i] === "a" && textoEncriptado[i + 1] === "i") {
+                textoDesencriptado += "a";
+                i += 1; // Avanza el índice para evitar duplicados
+            } else if (textoEncriptado[i] === "e" && textoEncriptado.substring(i, i + 5) === "enter") {
+                textoDesencriptado += "e";
+                i += 4;
+            } else if (textoEncriptado[i] === "i" && textoEncriptado.substring(i, i + 4) === "imes") {
+                textoDesencriptado += "i";
+                i += 3;
+            } else if (textoEncriptado[i] === "o" && textoEncriptado.substring(i, i + 4) === "ober") {
+                textoDesencriptado += "o";
+                i += 3;
+            } else if (textoEncriptado[i] === "u" && textoEncriptado.substring(i, i + 4) === "ufat") {
+                textoDesencriptado += "u";
+                i += 3;
+            } else {
+                textoDesencriptado += textoEncriptado[i];
+            }
+
+            document.getElementById("msj").value = textoDesencriptado;
+            document.getElementById("position").style.display = "none";
+            document.getElementById("btn-copiar").style.display = "block";
+            document.getElementById("texto").value = "";
+
+            let historialItem = document.createElement("li");
+            historialItem.textContent = "Texto Desencriptado: " + textoDesencriptado;
+            document.querySelector(".historial").appendChild(historialItem);
         }
+
     }
-    document.getElementById("msj").value = textoDesencriptado;
-    document.getElementById("position").style.display = "none";
-    document.getElementById("btn-copiar").style.display = "block";
-    }
-    console.log(textoDesencriptado);
+
+
+    return;
 }
+
 
 function copiarTexto() {
     let texto = document.getElementById("msj");
-    
+
     // Seleccionar el texto dentro del textarea
     texto.select();
+    document.execCommand("copy");
+
 }
 
+function aparecerDesaparecer() {
+    var circleXMark = document.getElementsByClassName("fa-circle-xmark")[0];
+    var caretDown = document.getElementsByClassName("fa-caret-down")[0];
 
+    if (document.getElementById("caja").style.display !== "block" && circleXMark.style.display !== "block") {
+        document.getElementById("caja").style.display = "block";
+        circleXMark.style.display = "block";
+        caretDown.style.display = "none";
+    } else {
+        document.getElementById("caja").style.display = "none";
+        circleXMark.style.display = "none";
+        caretDown.style.display = "block";
+    }
+
+    return;
+}
